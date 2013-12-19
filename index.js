59b2e215977877afafa5ffee519ed4262676211c
tounano <bitbucket@clickhere.co.il>
2013-12-19 15:10:15 +0200

    Implemented the module

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-/**
- * Created by tounano on 12/19/13.
- */
+var when = require("when");
+
+function asyncMethodToPromise (asyncMethod) {
+  var args = Array.prototype.slice.call(arguments)
+  var deferred = when.defer()
+
+  args.push(resultCallback)
+  callAsyncMethod(args.shift(), args)
+
+  return deferred.promise;
+
+  function callAsyncMethod(method, args) {
+    method.apply(this, args)
+  }
+
+  function resultCallback (err) {
+    if (err)
+      return deferred.reject(err)
+
+    return deferred.resolve(arguments[1])
+  }
+}
+
+function asyncMethodToPromiseWithArrayAsArgs(asyncMethod, args) {
+  var a = [asyncMethod].concat(args);
+  return asyncMethodToPromise.apply(this, a);
+}
+
+module.exports.asyncMethodToPromise = asyncMethodToPromise;
+module.exports.asyncMethodToPromiseWithArrayAsArgs = asyncMethodToPromiseWithArrayAsArgs;
--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,61 @@
+var moduleToTest  = "../index";
 var chai        = require("chai");
 var sinon       = require("sinon");
 var sinonChai   = require("sinon-chai");
-var when        = require("when");
 
-chai.use(sinonChai);
-var should = chai.should();
-var expect = chai.expect;
+chai.use(sinonChai)
+var should = chai.should()
+var expect = chai.expect
 
 var chaiAsPromised = require("chai-as-promised");
 
-chai.use(chaiAsPromised);
+chai.use(chaiAsPromised)
+describe("promisifier", function () {
+  var promisifier = require(moduleToTest)
+  var spy
+  var mock
 
-describe("Promisifier", function () {
-  it("should pass the first test without a problem", function () {
-    
+  beforeEach(function () {
+    spy = sinon.spy()
+    mock = sinon.mock()
   })
+
+  describe("#asyncMethodToPromise()", function () {
+    it("should return a promise", function () {
+      promisifier.asyncMethodToPromise(spy).should.have.property('then')
+    })
+    it("should call the callback that is passed as arg 1", function () {
+      promisifier.asyncMethodToPromise(spy)
+      spy.should.be.called
+    })
+    it("should reject the promise when error happens in the callback", function (done) {
+      promisifier.asyncMethodToPromise(createStubForAsyncMethod([new Error]))
+        .should.be.rejected.and.notify(done)
+    })
+    it("should resolve the promise with the 2nd argument from result callback", function (done) {
+      promisifier.asyncMethodToPromise(createStubForAsyncMethod([null, "GOOD"]))
+        .should.become("GOOD").and.notify(done);
+    })
+    it("should work with 2 args as well", function () {
+      promisifier.asyncMethodToPromise(mock.withArgs(1,2),1,2)
+      mock.verify()
+    })
+  })
+  describe("#asyncMethodToPromiseWithArrayAsArgs()", function () {
+    it("should return a promise", function () {
+      promisifier.asyncMethodToPromiseWithArrayAsArgs(createStubForAsyncMethod([null]))
+        .should.have.property("then")
+    })
+    it("should pass the arguments from the array to the async method", function () {
+      promisifier.asyncMethodToPromiseWithArrayAsArgs(mock.withArgs(1,2), [1,2])
+      mock.verify()
+    })
+  })
+
+  function createStubForAsyncMethod(argsToPassToResultCallback) {
+    return function (cb) {
+      var args = Array.prototype.slice.call(arguments)
+      args.pop().apply(this, argsToPassToResultCallback)
+    }
+  }
 })
